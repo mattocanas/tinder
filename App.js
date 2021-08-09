854b5d45851c5645e168c7d2b50300ccdd32dc04
Matthew Ocanas <matthewocanas@icloud.com>
2021-08-08 21:23:34 -0600

    added animation and gesture handling

--- App.js
+++ App.js
@@ -1,61 +1,62 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet, ImageBackground} from 'react-native';
+import 'react-native-gesture-handler';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from 'react-native';
+import Card from './src/components/Card';
+import users from './assets/data/users';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  useAnimatedGestureHandler,
+} from 'react-native-reanimated';
+import {PanGestureHandler} from 'react-native-gesture-handler';
 
 export default function App() {
+  const translateX = useSharedValue(0);
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+    ],
+  }));
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = translateX.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = context.startX + event.translationX;
+    },
+    onEnd: () => {
+      console.warn('touch ended');
+    },
+  });
+
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-        <ImageBackground
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
-          }}
-          style={styles.image}>
-          <View style={styles.cardInner}>
-            <Text style={styles.name}>Elon Musk</Text>
-            <Text style={styles.bio}>
-              {' '}
-              A dude with a rocket is looking for a gal with fuel
-            </Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.animatedCard, cardStyle]}>
+          <Card user={users[2]} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   pageContainer: {justifyContent: 'center', alignItems: 'center', flex: 1},
-  image: {
+  animatedCard: {
     width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-    justifyContent: 'flex-end',
-  },
-  card: {
-    width: '95%',
-    height: '70%',
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-    elevation: 11,
-  },
-  name: {
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  bio: {
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,
-  },
-  cardInner: {
-    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
