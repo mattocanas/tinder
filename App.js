fb763fd35bc953f006aef427343b96344f54220a
Matthew Ocanas <matthewocanas@icloud.com>
2021-08-11 17:00:28 -0600

    navigation done

--- App.js
+++ App.js
@@ -8,28 +8,64 @@ import {
   ImageBackground,
   Pressable,
   useWindowDimensions,
+  SafeAreaView,
 } from 'react-native';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import Homescreen from './src/screens/Homescreen';
 import MatchesScreen from './src/screens/MatchesScreen';
+import {color} from 'react-native-reanimated';
 
 export default function App() {
-  const onSwipeLeft = user => {
-    console.warn('swipe left', user.name);
-  };
+  const color = '#b5b5b5';
+  const activeColor = '#f76c6b';
 
-  const onSwipeRight = user => {
-    console.warn('swipe right', user.name);
-  };
+  const [activeScreen, setActiveScreen] = useState('HOME');
 
   return (
-    <View style={styles.pageContainer}>
-      {/* <Homescreen /> */}
-      <MatchesScreen />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.pageContainer}>
+        <View style={styles.topNavigation}>
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Fontisto
+              name="tinder"
+              size={30}
+              color={activeScreen == 'HOME' ? activeColor : color}
+            />
+          </Pressable>
+          <MaterialCommunityIcon
+            name="star-four-points"
+            size={30}
+            color={color}
+          />
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicon
+              name="ios-chatbubbles"
+              size={30}
+              color={activeScreen == 'CHAT' ? activeColor : color}
+            />
+          </Pressable>
+          <FontAwesome name="user" size={30} color={color} />
+        </View>
+
+        {activeScreen == 'HOME' && <Homescreen />}
+
+        {activeScreen == 'CHAT' && <MatchesScreen />}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   pageContainer: {justifyContent: 'center', alignItems: 'center', flex: 1},
+  root: {flex: 1},
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+  },
 });
--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -11,6 +11,9 @@ import {
 } from 'react-native';
 import Card from '../components/Card';
 import users from '../../assets/data/users';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 import AnimatedStack from '../components/AnimatedStack/index';
 
@@ -31,6 +34,27 @@ export default function Homescreen() {
         onSwipeRight={onSwipeRight}
         onSwipeLeft={onSwipeLeft}
       />
+      <View style={styles.icons}>
+        <View style={styles.button}>
+          <FontAwesome name="undo" size={30} color={'#f8d88b'} />
+        </View>
+
+        <View style={styles.button}>
+          <Entypo name="cross" size={30} color={'#f76c6b'} />
+        </View>
+
+        <View style={styles.button}>
+          <FontAwesome name="star" size={30} color={'#3ab4cc'} />
+        </View>
+
+        <View style={styles.button}>
+          <FontAwesome name="heart" size={30} color={'#4fcc94'} />
+        </View>
+
+        <View style={styles.button}>
+          <Ionicon name="flash" size={30} color={'#a65cd2'} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -41,5 +65,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     width: '100%',
+    backgroundColor: '#ededed',
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
   },
 });
